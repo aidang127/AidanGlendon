b1a8abbcd0f13234401305350261c95c6e19a02a
aidang127 <47786212+aidang127@users.noreply.github.com>
2021-01-12 12:27:52 -0500

    Added the email form

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,4 +171,10 @@
 
 		});
 
+
+// Form validator
+	var frmvalidator = new Validator(“contactform”);
+	frmvalidator.addValidation(“name”,”req”,”Please provide your name”);
+	frmvalidator.addValidation(“email”,”req”,”Please provide your email”);
+	frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”);
 })(jQuery);
